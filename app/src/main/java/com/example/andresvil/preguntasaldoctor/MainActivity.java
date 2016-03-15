8326c6d26fa2b5fab7a74636955e3504e6efe886
Andres Villarreal <andresvil@gmail.com>
2016-03-15 16:47:14 -0600

    add intent to PreguntaNueva activity and add spinner

--- app/src/main/java/com/example/andresvil/preguntasaldoctor/MainActivity.java
+++ app/src/main/java/com/example/andresvil/preguntasaldoctor/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.andresvil.preguntasaldoctor;
 
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -98,8 +99,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(getApplicationContext(), PreguntaNueva.class);
+                startActivity(i);
             }
         });
     }
--- app/src/main/java/com/example/andresvil/preguntasaldoctor/PreguntaNueva.java
+++ app/src/main/java/com/example/andresvil/preguntasaldoctor/PreguntaNueva.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,11 +36,21 @@ public class PreguntaNueva extends AppCompatActivity implements AdapterView.OnIt
         doctores.add("Neurólogo");
         doctores.add("Cardiólogo");
 
+        // Create adapter for spinner
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, doctores);
+
+        // Dropdown layout style
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Attach adapter to spinner
+        spinner.setAdapter(arrayAdapter);
+
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_SHORT).show();
     }
 
     @Override
