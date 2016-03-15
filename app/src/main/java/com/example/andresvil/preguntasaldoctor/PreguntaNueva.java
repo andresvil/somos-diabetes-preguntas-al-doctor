a8c6a87c62059092cead4d99a59f4e33fc84e120
Andres Villarreal <andresvil@gmail.com>
2016-03-15 17:55:01 -0600

    change  textView font

--- app/src/main/java/com/example/andresvil/preguntasaldoctor/PreguntaNueva.java
+++ app/src/main/java/com/example/andresvil/preguntasaldoctor/PreguntaNueva.java
@@ -1,5 +1,7 @@
 package com.example.andresvil.preguntasaldoctor;
 
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,11 +10,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class PreguntaNueva extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -22,6 +27,9 @@ public class PreguntaNueva extends AppCompatActivity implements AdapterView.OnIt
         setContentView(R.layout.activity_pregunta_nueva);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView eligeDoctor = (TextView) findViewById(R.id.eligeDoctorText);
+        EditText escribaTuPregunta = (EditText) findViewById(R.id.preguntaField);
 
         // Spinner element
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
@@ -44,6 +52,12 @@ public class PreguntaNueva extends AppCompatActivity implements AdapterView.OnIt
 
         // Attach adapter to spinner
         spinner.setAdapter(arrayAdapter);
+
+        // Load font and set TextViews to that font
+        AssetManager am = this.getApplicationContext().getAssets();
+        Typeface myFont = Typeface.createFromAsset(am, String.format(Locale.US, "fonts/%s", "AvenirLTStd-Roman_0.otf"));
+        eligeDoctor.setTypeface(myFont);
+        escribaTuPregunta.setTypeface(myFont);
 
     }
 
